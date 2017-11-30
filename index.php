6757b0515609a8402108f2f50e17504de48be53c
AxelZelenskiy <33211469+AxelZelenskiy@users.noreply.github.com>
2017-11-30 10:02:56 +0200

    testing commiting

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ require_once $_SERVER['DOCUMENT_ROOT']. '/Config/autoload.php';
 echo "<h2> index file</h2>";
 echo \App\Controllers\User::$hello . "</br>";
 echo \App\Models\User::$hello;
+echo "Test commit for github";
